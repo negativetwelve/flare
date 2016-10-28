aa5b1a2501415ea56a36c8457fa5cdca766cb362
Mark Miyashita <negativetwelve@gmail.com>
2016-10-28 07:43:27 -0700

    Add initial url class for flare-url parsing

---
+++ packages/flare-url/lib/flare/url.rb
@@ -0,0 +1,44 @@
+module Flare
+  class Url
+
+    #
+    # Base parse method which handles invalid uris.
+    #
+    def self.parsed(url)
+      Addressable::URI.heuristic_parse(url)
+    rescue Addressable::URI::InvalidURIError => error
+      nil
+    end
+
+    #
+    # Uses PublicSuffix to lookup validated domain names.
+    #
+    def self.parse_host(string)
+      PublicSuffix.parse(host(string))
+    end
+
+    #
+    # Returns if a url is valid after being parsed.
+    #
+    def self.is_valid?(url)
+      %w(http https).include?(scheme(url))
+    end
+
+    #
+    # Attempts to parse a string to get the `host` value from it. Typically
+    # this value is in the form of `google.com` or `inbox.google.com` with
+    # subdomains.
+    #
+    def self.host(string)
+      parsed(string)&.host
+    end
+
+    #
+    # Returns the scheme (http, https, ftp) for a url.
+    #
+    def self.scheme(url)
+      parsed(url)&.scheme
+    end
+
+  end
+end
